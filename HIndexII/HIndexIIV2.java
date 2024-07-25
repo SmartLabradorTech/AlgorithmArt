369abdf60836a2919f25c1fed8e9d2d7615540f3
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2024-07-24 21:50:18 -0700

    add a todo to simplify logic
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

---
+++ HIndexII/HIndexIIV2.java
@@ -0,0 +1,13 @@
+package HIndexII;
+
+public class HIndexIIV2 {
+
+    // TODO: simplify the binary search logic
+    public int hIndex(int[] citations) {
+
+        int length = citations.length;
+
+        return 0;
+    }
+
+}
