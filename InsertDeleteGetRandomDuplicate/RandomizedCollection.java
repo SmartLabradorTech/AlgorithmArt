3dd3cf3d1ac9d5627af00e498dba6c0dfd6de16b
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2024-07-25 14:31:48 -0700

    allow duplicate in the operations
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

--- InsertDeleteGetRandomDuplicate/RandomizedCollection.java
+++ InsertDeleteGetRandomDuplicate/RandomizedCollection.java
@@ -2,8 +2,11 @@ package InsertDeleteGetRandomDuplicate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 // https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/
 
@@ -11,7 +14,7 @@ public class RandomizedCollection {
 
     List<Integer> data;
 
-    Map<Integer, List<Integer>> map;
+    Map<Integer, Set<Integer>> map;
 
     public RandomizedCollection() {
 
@@ -21,19 +24,62 @@ public class RandomizedCollection {
     }
 
     public boolean insert(int val) {
-        boolean exists = false;
-        if (map.containsKey(val)) {
-            exists = true;
+        boolean exists = !map.containsKey(val);
+
+        data.add(val);
+
+        Set<Integer> indices = map.get(val);
+
+        if (indices == null) {
+            indices = new HashSet<>();
         }
 
+        indices.add(data.size() - 1);
+
+        map.put(val, indices);
+
+        return exists;
     }
 
     public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
 
+        Set<Integer> indices = map.get(val);
+
+        int pickedIndex = indices.iterator().next();
+
+        indices.remove(pickedIndex);
+
+        if (indices.isEmpty()) {
+            map.remove(val);
+        }
+
+        int allLastIndex = data.size() - 1;
+        int allLastElement = data.getLast();
+
+        if (pickedIndex == allLastIndex) {
+            data.removeLast();
+        } else {
+
+            data.set(pickedIndex, allLastElement);
+
+            map.get(allLastElement).remove(allLastIndex);
+            map.get(allLastElement).add(pickedIndex);
+
+            data.removeLast();
+        }
+
+        return true;
     }
 
     public int getRandom() {
+        Random random = new Random();
 
+        int index = random.nextInt(data.size());
+
+        return data.get(index);
     }
 
 }
