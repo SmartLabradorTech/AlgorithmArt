5c04777ccb5aaa8c7d5971ddd5b2db06727910cd
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2024-08-18 17:15:32 -0700

    prefix and suffix check
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

---
+++ CountPrefixAndSuffixPairsI/CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,41 @@
+package CountPrefixAndSuffixPairsI;
+
+// https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/description/
+
+public class CountPrefixAndSuffixPairsI {
+
+    public int countPrefixSuffixPairs(String[] words) {
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+
+            for (int j = i + 1; j < words.length; j++) {
+
+                if (isValid(words[i], words[j])) {
+
+                    count++;
+                }
+
+            }
+
+        }
+
+        return count;
+    }
+
+    private boolean isValid(String s1, String s2) {
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            if (i >= s2.length() || s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+
+            if (s2.length() - 1 - i < 0 || s1.charAt(s1.length() - 1 - i) != s2.charAt(s2.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
