3fdb01fc48f7480e68ba7628cb46a9a96e9ac28a
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2024-08-21 14:38:00 -0700

    restructure
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

---
+++ array/LongestCommonPrefix/LongestCommonPrefix.java
@@ -0,0 +1,10 @@
+package array.jumpgame.LongestCommonPrefix;
+// https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/
+
+public class LongestCommonPrefix {
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+
+        return 0;
+    }
+}
