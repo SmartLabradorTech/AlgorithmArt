b5f9590b4e4e0d8e6d8a1c7185f34cb84ef42868
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2024-08-24 20:41:19 -0700

    placeholder for kmp
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

---
+++ string/FirstOccurrence/FirstOccurrenceV2.java
@@ -0,0 +1,14 @@
+
+package string.FirstOccurrence;
+
+/**
+ * FirstOccurrenceV2
+ */
+public class FirstOccurrenceV2 {
+
+    public int strStr(String haystack, String needle) {
+
+        return 0;
+    }
+
+}
