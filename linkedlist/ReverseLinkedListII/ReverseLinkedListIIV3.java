c55430ba4bc887a77606055d13c483eee6e55900
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2024-11-12 21:55:03 -0800

    simple version of recursion
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

---
+++ linkedlist/ReverseLinkedListII/ReverseLinkedListIIV3.java
@@ -0,0 +1,34 @@
+package linkedlist.ReverseLinkedListII;
+
+public class ReverseLinkedListIIV3 {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if (left == right) {
+            return head;
+        }
+
+        if (left != 1) {
+            // the current node do not reverse.
+
+            ListNode subHead = reverseBetween(head.next, left - 1, right - 1);
+
+            head.next = subHead;
+
+            return head;
+        }
+
+        ListNode next = head.next;
+
+        ListNode subHead = reverseBetween(next, 1, right - 1);
+
+        ListNode temp = next.next;
+
+        head.next = temp;
+
+        next.next = head;
+
+        return subHead;
+    }
+
+}
