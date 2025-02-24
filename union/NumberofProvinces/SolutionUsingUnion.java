6eb34b72251d6a9f1f8f57b37dd95dfa078ac6cd
SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>
2025-02-23 21:05:02 -0800

    redo using union find class
    Signed-off-by: SmartLabradorTech <22583948+SmartLabradorTech@users.noreply.github.com>

---
+++ union/NumberofProvinces/SolutionUsingUnion.java
@@ -0,0 +1,72 @@
+package union.NumberofProvinces;
+
+// https://leetcode.com/problems/number-of-provinces/description/
+
+public class SolutionUsingUnion {
+
+    class UnionFind {
+
+        private int[] capital;
+
+        private int unionCount;
+
+        private int n;
+
+        public UnionFind(int[][] isConnected) {
+            unionCount = 0;
+
+            n = isConnected.length;
+            capital = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                capital[i] = i;
+            }
+
+            for (int i = 0; i < n; i++) {
+
+                for (int j = i + 1; j < n; j++) {
+
+                    if (isConnected[i][j] == 1) {
+                        union(i, j);
+                    }
+
+                }
+            }
+
+        }
+
+        public void union(int x, int y) {
+
+            int first = find(x);
+
+            int second = find(y);
+
+            if (first != second) {
+                capital[second] = first;
+
+                unionCount++;
+            }
+        }
+
+        public int find(int x) {
+
+            if (capital[x] == x) {
+                return x;
+            }
+
+            return find(capital[x]);
+        }
+
+        public int count() {
+            return n - unionCount;
+        }
+
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+
+        UnionFind uf = new UnionFind(isConnected);
+
+        return uf.count();
+    }
+}
